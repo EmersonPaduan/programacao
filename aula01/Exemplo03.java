f9319314592cc9f31826aa029419de0b4a4fdcc5
EmersonPaduan <emerson.paduan@gmail.com>
2022-02-25 21:52:41 -0300

    aula01

---
+++ aula01/Exemplo03.java
@@ -0,0 +1,12 @@
+public class Exemplo03 {
+    public static void main(String[] args) {
+        // TIPO nome;
+        int idade; 
+
+        idade = 23; // atribuição, guardar o valor na variável
+        idade = 45 + 34;
+
+        System.out.println("Sua idade é " + idade);
+
+    }
+}
